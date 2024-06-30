abc2981734507e7fcf8e569f5aca1da61c64920b
HariChandana5755 <88962971+HariChandana5755@users.noreply.github.com>
2024-06-30 13:41:50 +0000

    container config done

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,23 @@
-console.log("hari");
+//variabes let const
+let val="hari";
+val="chandana";
+let name="hari chandana";
+console.log(val);
+const num=222;
+let numb=null;
+let n;
+//num=87;
+console.log(num);
+console.table([val, name,num]);
+
+// number, bigint, string, boolean, null, undefined, symbol, object (datatypes)
+//typeof as keyword and as method typeof()
+ console.log(typeof name);
+ console.log(typeof num);
+ console.log(typeof numb);
+ console.log(typeof n);
+
+let value=String(num);
+console.log(value);
+
+
